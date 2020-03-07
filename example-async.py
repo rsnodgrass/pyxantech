3d2b2b0232742ff0f9ec251a4b768cb0561bd3b0
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-03-06 21:30:27 -0800

    updated async

--- example-async.py
+++ example-async.py
@@ -8,8 +8,21 @@ import time
 import asyncio
 import argparse
 import serial
+import logging
+import sys
 
 from pyxantech import get_async_amp_controller, XANTECH8, MONOPRICE6
+
+####----------------------------------------
+LOG = logging.getLogger()
+LOG.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+LOG.addHandler(handler)
+####----------------------------------------
 
 parser = argparse.ArgumentParser(description='Xantech RS232 client example (asynchronous)')
 parser.add_argument('--tty', help='/dev/tty to use (e.g. /dev/tty.usbserial-A501SGSZ)', required=True)
