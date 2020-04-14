ac96028aec57cf92fe5d6165bc1fec78c9b541dd
Ryan Snodgrass <rsnodgrass@gmail.com>
2020-04-14 08:21:05 -0700

    moved all device and protocol config to yaml

--- pyxantech/__init__.py
+++ pyxantech/__init__.py
@@ -4,205 +4,30 @@ import logging
 import re
 import time
 import serial
+import os
+import yaml
 
 from functools import wraps
 from threading import RLock
 
+from .config import (DEVICE_CONFIG, PROTOCOL_CONFIG, RS232_RESPONSE_PATTERNS, get_with_log, pattern_to_dictionary)
 from .protocol import get_rs232_async_protocol
 
 LOG = logging.getLogger(__name__)
 
-MONOPRICE6 = 'monoprice6'   # Monoprice 6-zone amplifier (plus Dayton Audio 6-zone amplifiers are idential to Monoprice)
-XANTECH8   = 'xantech8'     # Xantech 8-zone amplifier
-ZPR68      = 'zpr68'        # Xantech ZPR68 (*NOT IMPLEMENTED*)
-SUPPORTED_AMP_TYPES = [ MONOPRICE6, XANTECH8 ]
-
-# NOTE: these ranges are different for each amp type
-MAX_BALANCE = 20
-MAX_BASS = 14
-MAX_TREBLE = 14
-MAX_VOLUME = 38
-
+MONOPRICE6 = "monoprice6" # hardcoded for backwards compatibility
 BAUD_RATES = [ 9600, 19200, 38400, 57600, 115200 ]
 
-DEFAULT_SERIAL_CONFIG = {
-    'baudrate':      9600,
-    'bytesize':      serial.EIGHTBITS,
-    'parity':        serial.PARITY_NONE,
-    'stopbits':      serial.STOPBITS_ONE,
-    'timeout':       1.0,
-    'write_timeout': 1.0
-}
+SUPPORTED_AMP_TYPES = DEVICE_CONFIG.keys()
 
 CONF_SERIAL_CONFIG='rs232'
 
-# FIXME: move this all to yaml configuration files (like the Anthem AV integration)
+def get_device_config(amp_type, key):
+    return get_with_log(amp_type, DEVICE_CONFIG[amp_type], KeyboardInterrupt)
 
-# technically zone = {amp_number}{zone_num_within_amp_1-6} (e.g. 11 = amp number 1, zone 1)
-RS232_COMMANDS = {
-    MONOPRICE6: {
-        'zone_status':   '?{zone}',
-
-        'set_power':     '<{zone}PR{on_off:02}',    # on_off: 1 = on; 0 = off
-        'power_on':      '<{zone}PR01',
-        'power_off':     '<{zone}PR00',
-
-        'set_mute':      '<{zone}MU{on_off:02}',
-        'mute_on':       '<{zone}MU01',
-        'mute_off':      '<{zone}MU00',
-
-        'set_volume':    '<{zone}VO{volume:02}',   # volume: 0-38
-        'set_treble':    '<{zone}TR{treble}:02}',  # treble: 0-14
-        'set_bass':      '<{zone}BS{bass:02}',     # bass: 0-14
-        'set_balance':   '<{zone}BL{balance:02}',  # balance: 0-20
-        'set_source':    '<{zone}CH{source:02}'    # source: 0-6
-    },
-
-    XANTECH8: {
-        'set_power':     "!{zone}PR{power}+",
-        'power_on':      "!{zone}PR1+",
-        'power_off':     "!{zone}PR0+",
-        'all_zones_off': '!AO+',
-
-        'set_mute':      '!{zone}MU{mute}+',
-        'mute_on':       '!{zone}MU1+',
-        'mute_off':      '!{zone}MU0+',
-        'mute_toggle':   '!{zone}MT+',
-
-        'set_volume':    '!{zone}VO{volume:02}+',    # volume: 0-38
-        'volume_up':     '!{zone}VI+',
-        'volume_down':   '!{zone}VD+',
-
-        'set_source':    '!{zone}SS{source}+',      # source (no leading zeros)
-
-        'set_bass':      '!{zone}BS{bass:02}+',     # bass: 0-14
-        'bass_up':       '!{zone}BI+',
-        'bass_down':     '!{zone}BD+',
-
-        'set_balance':   '!{zone}BL{balance:02}+',     # balance: 0-20
-        'balance_left':  '!{zone}BL+',
-        'balance_right': '!{zone}BR+',
-
-        'set_treble':    '!{zone}TR{treble:02}+',     # treble: 0-14
-        'treble_up':     '!{zone}TI+',
-        'treble_down':   '!{zone}TD+',
-
-        'disable_activity_updates': '!ZA0+',
-        'disable_status_updates':   '!ZP0+',
-        'power_toggle':  '!{zone}PT+',
-
-        # queries
-        'zone_status':    '?{zone}ZD+',
-        'zone_details':   '?{zone}ZD+',
-        'source_status':  '?{zone}SS+',
-        'volume_status':  '?{zone}VO+',
-        'mute_status':    '?{zone}MU+',
-        'power_status':   '?{zone}PR+',
-        'treble_status':  '?{zone}TR+',
-        'bass_status':    '?{zone}BS+',
-        'balance_status': '?{zone}BA+',
-
-        # FIXME: these aren't documented, do they work?
-        'set_activity_updates': '!ZA{activity_updates}+',      # on_off: 1 = on; 0 = off
-        'set_status_updates':   '!ZP{status_updates}+',      # on_off: 1 = on; 0 = off
-    },
-
-    ZPR68: {
-        'power_status':     '?{zone:02}C=', # Y or N
-        'power_on':         '!{zone:02}:CY+', 
-        'power_off':        '!{zone:02}:CN+',
-        'all_zones_off':    '!00CN+',
-
-        'volume_status':    '?{zone:02}V=',
-        'set_volume':       '!{zone:02}V{volume:02}+',
-        'volume_up':        '!{zone:02}LU+',
-        'volume_down':      '!{zone:02}LD+',
-
-        'mute_status':      '?{zone:02}M=', # Y or N
-        'mute_on':          '!{zone:02}QY+',
-        'mute_off':         '!{zone:02}QN+',
-        'mute_all_on':      '!00QY+',
-        'mute_all_off':     '!00QN+',
-
-        'source_status':    '?{zone:02}I=',
-        'set_source':       '!{zone:02}I{source}+',
-        'set_source_all':   '!00I{source}+',
-
-        'zone_status':      'Z{zone:02}',
-
-        'set_treble':       '!{zone:02}T{treble:02}+',
-
-        'treble_status':    '?{zone:02}T=',
-        'bass_status':      '?{zone:02}B=',
-    }
-}
-
-RS232_RESPONSES = {
-    MONOPRICE6: {
-        'zone_status':    "#>(?P<zone>\d\d)(?P<power>[01]{2})(?P<source>[01]{2})(?P<mute[01]{2})(?P<do_not_disturb[01])(?P<volume>\d\d)(?P<treble>\d\d)(?P<bass>\d\d)(?P<balance>\d\d)(?P<source>\d\d)(?P<keypad>\d\d)",
-    },
-
-    XANTECH8: {
-        'zone_status':    "#(?P<zone>\d+)ZS PR(?P<power>[01]) SS(?P<source>\d+) VO(?P<volume>\d+) MU(?P<mute>[01]) TR(?P<treble>\d+) BS(?P<bass>\d+) BA(?P<balance>\d+) LS(?P<linked>[01]) PS(?P<paged>[01])\+",
-                          # Example:  #1ZS PR0 SS1 VO0 MU1 TR7 BS7 BA32 LS0 PS0+
-        'power_status':   "\?(?P<zone>\d+)PR(?P<power[01])\+",
-        'source_status':  "\?(?P<zone>\d+)SS(?P<source>[1-8])\+",
-        'volume_status':  "\?(?P<zone>\d+)VO(?P<volume>\d+)\+",
-        'mute_status':    "\?(?P<zone>\d+)MU(?P<mute>[01])\+",
-        'treble_status':  "\?(?P<zone>\d+)TR(?P<treble>\d+)\+",
-        'bass_status':    "\?(?P<zone>\d+)BS(?P<bass>\d+)\+",
-        'balance_status': "\?(?P<zone>\d+)BA(?P<balance>\d+)\+",
-    }
-}
-
-AMP_TYPE_CONFIG ={
-    MONOPRICE6: {
-        'rs232':           DEFAULT_SERIAL_CONFIG,
-        'protocol': {
-            'eol': b'\r\n#',
-            'command_eol': "\r"
-        },
-        'max_amps':        3,
-        'sources':         [ 1, 2, 3, 4, 5, 6 ],
-        'zones':           [ 11, 12, 13, 14, 15, 16,           # main amp 1    (e.g. 15 = amp 1, zone 5)
-                             21, 22, 23, 24, 25, 26,           # linked amp 2  (e.g. 23 = amp 2, zone 3)
-                             31, 32, 33, 34, 35, 36 ],         # linked amp 3
-        'restore_zone':    [ 'set_power', 'set_source', 'set_volume', 'set_mute', 'set_bass', 'set_balance', 'set_treble' ],
-        'restore_success': "OK\r"
-    },
-
-    # NOTE: Xantech MRC88 seems to indicate zones are 1..8, or 1..16 if expanded; perhaps this scheme for multi-amps changed
-    XANTECH8: {
-        'rs232':           DEFAULT_SERIAL_CONFIG,
-        'protocol': {
-            'eol': b'\r',     # replies: \r
-            'command_eol': '' # sending: '+'
-        },
-        'max_amps':        3,
-        'sources':         [ 1, 2, 3, 4, 5, 6, 7, 8 ],
-        'zones':           [ 1, 2, 3, 4, 5, 6, 7, 8,
-                             11, 12, 13, 14, 15, 16, 17, 18,   # main amp 1    (e.g. 15 = amp 1, zone 5)
-                             21, 22, 23, 24, 25, 26, 27, 28,   # linked amp 2  (e.g. 23 = amp 2, zone 3)
-                             31, 32, 33, 34, 35, 36, 37, 38 ],  # linked amp 3
-        'restore_zone':    [ 'set_power', 'set_source', 'set_volume', 'set_mute', 'set_bass', 'set_balance', 'set_treble' ],
-        'restore_success': "OK\r"
-    },
-
-    ZPR68: {
-        'rs232':           DEFAULT_SERIAL_CONFIG,
-        'protocol': {
-            'eol': b'\r',     # replies: \r
-            'command_eol': '' # sending: '='
-        }
-    }
-}
-
-def _get_config(amp_type: str, key: str):
-    config = AMP_TYPE_CONFIG.get(amp_type)
-    if config:
-        return config.get(key)
-    LOG.error("Invalid amp type '%s' config key '%s'; returning None", amp_type, key)
-    return None
+def get_protocol_config(amp_type, key):
+    protocol = get_device_config(amp_type, 'protocol')
+    return PROTOCOL_CONFIG[protocol].get(key)
 
 # FIXME: populate based on dictionary, not positional
 class ZoneStatus(object):
@@ -230,7 +55,7 @@ class ZoneStatus(object):
         if not string:
             return None
 
-        pattern = RS232_RESPONSES[amp_type].get('zone_status')
+        pattern = RS232_RESPONSE_PATTERNS[amp_type].get('zone_status')
         match = re.search(pattern, string)
         if not match:
             LOG.debug("Could not pattern match zone status '%s' with '%s'", string, pattern)
@@ -238,8 +63,6 @@ class ZoneStatus(object):
 
         return ZoneStatus(match.groupdict())
 
-# FIXME: for Xantech the zones can be 11..18, 21..28, 31..38; perhaps split this as;
-#   zone_status(self, zone: int, amp_num: int = 1)  with default amp_num
 class AmpControlBase(object):
     """
     AmpliferControlBase amplifier interface
@@ -317,39 +140,18 @@ class AmpControlBase(object):
         raise NotImplemented()
     
 
-def _pattern_to_dictionary(pattern: str, text: str) -> dict:
-    result = pattern.match(text)
-    if not result:
-        LOG.error(f"Could not parse '{text}' with pattern '{pattern}'")
-        return None
-
-    d = result.groupdict()
-
-    # FIXME: for safety, we may want to limit which keys this applies to
-    # replace and 0 or 1 with True or False
-    boolean_keys = [ 'power', 'mute', 'pa' ]
-    for k, v in d.items():
-        if k in boolean_keys:
-            if v == '0':
-                d[k] = False
-            elif v == '1':
-                d[k] = True
-
-    return d
-
-
 def _command(amp_type: str, format_code: str, args = {}):
-    protocol_config = _get_config(amp_type, 'protocol')
-    eol = protocol_config.get('command_eol')
-    command = RS232_COMMANDS[amp_type].get(format_code) + eol
+    eol = get_protocol_config(amp_type, 'eol')
+    rs232_commands = get_protocol_config(amp_type, 'commands')
+    command = rs232_commands.get(format_code) + eol
     return command.format(**args).encode('ascii')
 
 def _zone_status_cmd(amp_type, zone: int) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
+    assert zone in get_device_config(amp_type, 'zones')
     return _command(amp_type, 'zone_status', args = { 'zone': zone })
 
 def _set_power_cmd(amp_type, zone: int, power: bool) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
+    assert zone in get_device_config(amp_type, 'zones')
     if power:
         LOG.info(f"Powering on {amp_type} zone {zone}")
         return _command(amp_type, 'power_on', { 'zone': zone })
@@ -358,7 +160,7 @@ def _set_power_cmd(amp_type, zone: int, power: bool) -> bytes:
         return _command(amp_type, 'power_off', { 'zone': zone })
 
 def _set_mute_cmd(amp_type, zone: int, mute: bool) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
+    assert zone in get_device_config(amp_type, 'zones')
     if mute:
         LOG.info(f"Muting {amp_type} zone {zone}")
         return _command(amp_type, 'mute_on', { 'zone': zone })
@@ -367,32 +169,36 @@ def _set_mute_cmd(amp_type, zone: int, mute: bool) -> bytes:
         return _command(amp_type, 'mute_off', { 'zone': zone })
     
 def _set_volume_cmd(amp_type, zone: int, volume: int) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
-    volume = int(max(0, min(volume, MAX_VOLUME)))
+    assert zone in get_device_config(amp_type, 'zones')
+    max_volume = get_device_config(amp_type, 'max_volume')
+    volume = int(max(0, min(volume, max_volume)))
     LOG.info(f"Setting volume {amp_type} zone {zone} to {volume}")
     return _command(amp_type, 'set_volume', args = { 'zone': zone, 'volume': volume })
 
 def _set_treble_cmd(amp_type, zone: int, treble: int) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
-    treble = int(max(0, min(treble, MAX_TREBLE)))
+    assert zone in get_device_config(amp_type, 'zones')
+    max_treble = get_device_config(amp_type, 'max_treble')
+    treble = int(max(0, min(treble, max_treble)))
     LOG.info(f"Setting treble {amp_type} zone {zone} to {treble}")
     return _command(amp_type, 'set_treble', args = { 'zone': zone, 'treble': treble })
 
 def _set_bass_cmd(amp_type, zone: int, bass: int) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
-    bass = int(max(0, min(bass, MAX_BASS)))
+    assert zone in get_device_config(amp_type, 'zones')
+    max_bass = get_device_config(amp_type, 'max_bass')
+    bass = int(max(0, min(bass, max_bass)))
     LOG.info(f"Setting bass {amp_type} zone {zone} to {bass}")
     return _command(amp_type, 'set_bass', args = { 'zone': zone, 'bass': bass })
 
 def _set_balance_cmd(amp_type, zone: int, balance: int) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
-    balance = max(0, min(balance, MAX_BALANCE))
+    assert zone in get_device_config(amp_type, 'zones')
+    max_balance = get_device_config(amp_type, 'max_balance')
+    balance = max(0, min(balance, max_balance))
     LOG.info(f"Setting balance {amp_type} zone {zone} to {balance}")
     return _command(amp_type, 'set_balance', args = { 'zone': zone, 'balance': balance })
 
 def _set_source_cmd(amp_type, zone: int, source: int) -> bytes:
-    assert zone in _get_config(amp_type, 'zones')
-    assert source in _get_config(amp_type, 'sources')
+    assert zone in get_device_config(amp_type, 'zones')
+    assert source in get_device_config(amp_type, 'sources')
     LOG.info(f"Setting source {amp_type} zone {zone} to {source}")
     return _command(amp_type, 'set_source', args = { 'zone': zone, 'source': source })
 
@@ -422,11 +228,11 @@ def get_amp_controller(amp_type: str, port_url, serial_config_overrides={}):
     class AmpControlSync(AmpControlBase):
         def __init__(self, amp_type, port_url, serial_config_overrides):
             self._amp_type = amp_type
-            config = AMP_TYPE_CONFIG[amp_type]
+            config = DEVICE_CONFIG[amp_type]
 
             # allow overriding the default serial port configuration, in case the user has changed
             # settings on their amplifier (e.g. increased the default baudrate)
-            serial_config = config.get('rs232')
+            serial_config = config.get(CONF_SERIAL_CONFIG)
             if serial_config_overrides:
                 LOG.debug(f"Overiding serial port config for {port_url}: {serial_config_overrides}")
                 serial_config.update(serial_config_overrides)
@@ -450,8 +256,7 @@ def get_amp_controller(amp_type: str, port_url, serial_config_overrides={}):
             self._port.write(request)
             self._port.flush()
 
-            protocol_config = _get_config(self._amp_type, 'protocol')
-            eol = protocol_config.get('eol')
+            eol = get_protocol_config(amp_type, 'eol')
             len_eol = len(eol)
 
             # receive
@@ -514,13 +319,13 @@ def get_amp_controller(amp_type: str, port_url, serial_config_overrides={}):
         def restore_zone(self, status: dict):
             zone = status['zone']
             amp_type = self._amp_type
-            success = AMP_TYPE_CONFIG[amp_type].get('restore_success')
+            success = get_protocol_config(amp_type, 'restore_success')
             #LOG.debug(f"Restoring amp {amp_type} zone {zone} from {status}")
 
             # FIXME: fetch current status first and only call those that changed
 
             # send all the commands necessary to restore the various status settings to the amp
-            restore_commands = AMP_TYPE_CONFIG[amp_type].get('restore_zone')
+            restore_commands = get_protocol_config(amp_type, 'restore_zone')
             for command in restore_commands:
                 result = self._send_request( _command(amp_type, command, status) )
                 if result != success:
@@ -552,8 +357,9 @@ async def get_async_amp_controller(amp_type, port_url, loop, serial_config_overr
         LOG.error("Unsupported amplifier type '%s'", amp_type)
         return None
 
-    config = AMP_TYPE_CONFIG[amp_type]
-    protocol_config = config.get('protocol')
+    serial_config = DEVICE_CONFIG[amp_type].get('rs232')
+    protocol = get_with_log(amp_type, DEVICE_CONFIG, 'protocol')
+    protocol_config = PROTOCOL_CONFIG[protocol]
 
     lock = asyncio.Lock()
 
@@ -565,18 +371,16 @@ async def get_async_amp_controller(amp_type, port_url, loop, serial_config_overr
         return wrapper
 
     class AmpControlAsync(AmpControlBase):
-        def __init__(self, config, amp_type, protocol):
-            self._config = config
+        def __init__(self, amp_type, serial_config, protocol):
             self._amp_type = amp_type
+            self._serial_config = serial_config
             self._protocol = protocol
 
         @locked_coro
         async def zone_status(self, zone: int):
             cmd = _zone_status_cmd(self._amp_type, zone)
-            LOG.info(f"Calling command {cmd}")
-            string = await self._protocol.send(cmd)
-            LOG.info(f"For command {cmd} received {string}")
-            return ZoneStatus.from_string(self._amp_type, string).dict
+            status_string = await self._protocol.send(cmd)
+            return ZoneStatus.from_string(self._amp_type, status_string).dict
 
         @locked_coro
         async def set_power(self, zone: int, power: bool):
@@ -614,11 +418,11 @@ async def get_async_amp_controller(amp_type, port_url, loop, serial_config_overr
         async def restore_zone(self, status: dict):
             zone = status['zone']
             amp_type = self._amp_type
-            success = self._config.get('restore_success')
+            success = get_protocol_config(amp_type, 'restore_success')
             #LOG.debug(f"Restoring amp {amp_type} zone {zone} from {status}")
 
             # send all the commands necessary to restore the various status settings to the amp
-            restore_commands = self._config.get('restore_zone')
+            restore_commands = get_protocol_config(amp_type, 'restore_zone')
             for command in restore_commands:
                 result = await self._protocol._send( _command(amp_type, command, status) )
                 if result != success:
@@ -627,10 +431,10 @@ async def get_async_amp_controller(amp_type, port_url, loop, serial_config_overr
 
     # allow overriding the default serial port configuration, in case the user has changed
     # settings on their amplifier (e.g. increased the default baudrate)
-    serial_config = config.get('rs232')
+    serial_config = serial_config.get(CONF_SERIAL_CONFIG)
     if serial_config_overrides:
         LOG.debug(f"Overiding serial port config for {port_url}: {serial_config_overrides}")
         serial_config.update(serial_config_overrides)
 
     protocol = await get_rs232_async_protocol(port_url, serial_config, protocol_config, loop)
-    return AmpControlAsync(config, amp_type, protocol)
+    return AmpControlAsync(amp_type, serial_config, protocol)
--- pyxantech/protocol.py
+++ pyxantech/protocol.py
@@ -1,28 +1,44 @@
 import logging
 
+import time
+import asyncio
 import functools
 import serial
-
-import asyncio
-#import serial_asyncio
+from serial_asyncio import create_serial_connection
 
 LOG = logging.getLogger(__name__)
 
-CONF_EOL = 'eol'
+CONF_EOL = 'command_eol'
+CONF_THROTTLE_RATE = 'min_time_between_commands'
 
-async def get_rs232_async_protocol(serial_port_url, serial_config, protocol_config, loop):
-    import serial_asyncio
+async def get_async_rs232_protocol(serial_port_url, serial_config, protocol_config, loop):
+
+    lock = asyncio.Lock()
+
+    def locked_coro(coro):
+        """While this is asynchronous, ensure only a single, ordered command is sent to RS232 at a time (non-reentrant lock)"""
+        @wraps(coro)
+        async def wrapper(*args, **kwargs):
+            LOG.debug("Waiting on coro lock: %s %s", args, kwargs)
+            with (await lock):
+                LOG.debug("Lock acquired! %s %s", args, kwargs)
+                return (await coro(*args, **kwargs))
+        return wrapper
 
     class RS232ControlProtocol(asyncio.Protocol):
-        def __init__(self, serial_port_url, serial_config, protocol_config, loop):
+        def __init__(self, serial_port_url, protocol_config, loop):
             super().__init__()
 
             self._serial_port_url = serial_port_url
-            self._serial_config = serial_config
-            self._timeout = self._serial_config.get('timeout')
-
-            self._protocol_config = protocol_config
+            self._config = protocol_config
             self._loop = loop
+
+            self._timeout = self._config.get('timeout')
+            if self._timeout is None:
+                self._timeout = 1.0 # default to 1 second if None
+            LOG.info(f"Timeout set to {self._timeout}")
+            
+            self._last_send = time.time() - 1
 
             self._lock = asyncio.Lock()
             self._transport = None
@@ -35,43 +51,72 @@ async def get_rs232_async_protocol(serial_port_url, serial_config, protocol_conf
             LOG.debug(f"Port {self._serial_port_url} opened {self._transport}")
 
         def data_received(self, data):
+#            LOG.debug(f"Received from {self._serial_port_url}: {data}")
             asyncio.ensure_future(self._q.put(data), loop=self._loop)
 
         def connection_lost(self, exc):
-            LOG.info(f"Port {self._serial_port_url} closed")
+            LOG.debug(f"Port {self._serial_port_url} closed")
 
-        async def send(self, request: bytes, skip=0):
+#        @locked_coro
+        async def send(self, request: bytes, wait_for_reply=True, skip=0):
+            data = bytearray()
+            eol = self._config[CONF_EOL].encode('ascii')
+            min_time_between_commands = self._config[CONF_THROTTLE_RATE]
+
             await self._connected.wait()
-            result = bytearray()
-
-            eol = self._protocol_config.get(CONF_EOL)
-            if eol == None:
-                eol = '\n'
-            len_eol = len(eol)
 
             # only one write/read at a time
             with (await self._lock):
-                # clear all waiting to be received data before sending the command
+                # clear all buffers of any data waiting to be read before sending the request
                 self._transport.serial.reset_output_buffer()
                 self._transport.serial.reset_input_buffer()
                 while not self._q.empty():
                     self._q.get_nowait()
 
+                # throttle the number of RS232 sends per second to avoid causing timeouts
+                delta_since_last_send = time.time() - self._last_send
+
+                if delta_since_last_send < 0:
+                    delay = -1 * delta_since_last_send
+                    LOG.debug(f"Sleeping {delay} seconds until sending another RS232 request as {self._name} is powering up")
+                    await asyncio.sleep(delay)
+
+                elif delta_since_last_send < min_time_between_commands:
+                    delay = min(max(0, min_time_between_commands - delta_since_last_send), min_time_between_commands)
+                    await asyncio.sleep(delay)
+
                 # send the request
+                LOG.debug("Sending RS232 data %s", request)
+                self._last_send = time.time()
                 self._transport.write(request)
+
+                # special case for power on, since the Anthem units can't accept more RS232 requests
+                # for many seconds after initial powering up
+                if request in [ "P1P1\n", "P2P1\n", "P1P3\n" ]:
+                    self._last_send = time.time() + self._config['delay_after_power_on']
+
+                if not wait_for_reply:
+                    return
 
                 # read the response
                 try:
                     while True:
-                        result += await asyncio.wait_for(self._q.get(), self._timeout, loop=self._loop)
-                        if len(result) > skip and result[-len_eol:] == eol:
-                            ret = bytes(result)
-                            LOG.debug('Received "%s"', ret)
-                            return ret.decode('ascii')
+                        data += await asyncio.wait_for(self._q.get(), self._timeout, loop=self._loop)
+#                        LOG.debug("Partial receive %s", bytes(data).decode('ascii'))
+                        if eol in data:
+                            # only return the first line
+                            result_lines = data.split(eol)
+                            if len(result_lines) > 1:
+                                LOG.debug("Multiple response lines, ignore all but the first: %s", result_lines)
+
+                            result = result_lines[0].decode('ascii')
+#                            LOG.debug('Received "%s"', result)
+                            return result
                 except asyncio.TimeoutError:
-                    LOG.error("Timeout receiving response for '%s': received='%s'", request, result)
+                    LOG.error("Timeout receiving response for '%s': received='%s'", request, data)
                     raise
 
-    factory = functools.partial(RS232ControlProtocol, serial_port_url, serial_config, protocol_config, loop)
-    _, protocol = await serial_asyncio.create_serial_connection(loop, factory, serial_port_url, **serial_config)
+    factory = functools.partial(RS232ControlProtocol, serial_port_url, protocol_config, loop)
+    LOG.debug(f"Creating RS232 connection to {serial_port_url}: {serial_config}")
+    _, protocol = await create_serial_connection(loop, factory, serial_port_url, **serial_config)
     return protocol
