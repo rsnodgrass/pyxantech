0a3701139a931d1b1cee09f76887e6699cc34d4c
hello@theauthtoken.com <rsnodgrass@gmail.com>
2022-11-06 14:24:25 -0800

    no code changes - reformatted with black

--- example.py
+++ example.py
@@ -4,20 +4,27 @@
 #   ./example-async.py --help
 #   ./example.py --tty /dev/tty.usbserial-A501SGSZ
 
-import argparse                                                                                             
+import argparse
 import time
 
 from pyxantech import get_amp_controller
 
-parser = argparse.ArgumentParser(description='Xantech RS232 client example')
-parser.add_argument('--tty', help='/dev/tty to use (e.g. /dev/tty.usbserial-A501SGSZ)', required=True)
-parser.add_argument('--model', default='xantech8', help=f"model (e.g. xantech8, monoprice6)" )
-parser.add_argument('--baud',type=int,default=9600,help='baud rate (9600, 14400, 19200, 38400, 57600, 115200)')
+parser = argparse.ArgumentParser(description="Xantech RS232 client example")
+parser.add_argument(
+    "--tty", help="/dev/tty to use (e.g. /dev/tty.usbserial-A501SGSZ)", required=True
+)
+parser.add_argument(
+    "--model", default="xantech8", help=f"model (e.g. xantech8, monoprice6)"
+)
+parser.add_argument(
+    "--baud",
+    type=int,
+    default=9600,
+    help="baud rate (9600, 14400, 19200, 38400, 57600, 115200)",
+)
 args = parser.parse_args()
 
-serial_config = {
-    'baudrate': args.baud
-}
+serial_config = {"baudrate": args.baud}
 
 zone = 1
 amp = get_amp_controller(args.model, args.tty, serial_config_overrides=serial_config)
@@ -25,13 +32,13 @@ amp = get_amp_controller(args.model, args.tty, serial_config_overrides=serial_co
 # save the status for all zones before modifying
 zone_status = {}
 for zone in range(1, 9):
-    zone_status[zone] = amp.zone_status(zone) # save current status for all zones
+    zone_status[zone] = amp.zone_status(zone)  # save current status for all zones
     print(f"Zone {zone} status: {zone_status[zone]}")
 
 amp.all_off()
 exit
 
-#for zone in range(1, 9):
+# for zone in range(1, 9):
 #    amp.set_power(zone, True)
 #    amp.set_mute(zone, False)
 #    print(f"Zone {zone} status: {amp.zone_status(zone)}")
@@ -41,14 +48,11 @@ amp.set_source(1, source)
 
 # restore zones back to their original states
 for zone in range(1, 9):
-    amp.restore_zone( zone_status[zone ])
-
-
-
+    amp.restore_zone(zone_status[zone])
 
 
 def knight_rider(amp, number_of_times):
-    for _ in range (1, number_of_times + 1):
+    for _ in range(1, number_of_times + 1):
         for zone in range(1, 9):
             amp.set_power(zone, True)
             amp.set_power(zone, False)
@@ -56,4 +60,6 @@ def knight_rider(amp, number_of_times):
         for zone in range(-7, -1):
             amp.set_power(-1 * zone, True)
             amp.set_power(-1 * zone, False)
+
+
 # knight_rider(amp, 2)
--- pyxantech/config.py
+++ pyxantech/config.py
@@ -9,11 +9,12 @@ LOG = logging.getLogger(__name__)
 DEVICE_CONFIG = {}
 PROTOCOL_CONFIG = {}
 
+
 def _load_config(config_file):
     """Load the amp series configuration"""
 
-#    LOG.debug(f"Loading {config_file}")
-    with open(config_file, 'r') as stream:
+    #    LOG.debug(f"Loading {config_file}")
+    with open(config_file, "r") as stream:
         try:
             config = yaml.load(stream, Loader=yaml.FullLoader)
             return config[0]
@@ -21,37 +22,40 @@ def _load_config(config_file):
             LOG.error(f"Failed reading config {config_file}: {exc}")
             return None
 
+
 def _load_config_dir(directory):
     config_tree = {}
 
     for filename in os.listdir(directory):
-#        try:
-          if filename.endswith('.yaml'):
-                series = filename.split('.yaml')[0]
-                config = _load_config(os.path.join(directory, filename))
-                if config:
-                    config_tree[series] = config
-#        except Exception as e:
-#            LOG.warning(f"Failed parsing {filename}; ignoring that configuration file: {e}")
+        #        try:
+        if filename.endswith(".yaml"):
+            series = filename.split(".yaml")[0]
+            config = _load_config(os.path.join(directory, filename))
+            if config:
+                config_tree[series] = config
+    #        except Exception as e:
+    #            LOG.warning(f"Failed parsing {filename}; ignoring that configuration file: {e}")
 
     return config_tree
+
 
 def pattern_to_dictionary(protocol_type, match, source_text: str) -> dict:
     """Convert the pattern to a dictionary, replacing 0 and 1's with True/False"""
     LOG.info(f"Pattern matching {source_text} {match}")
     d = match.groupdict()
-            
+
     # type convert any pre-configured fields
     # TODO: this could be a lot more efficient LOL
-    boolean_fields = PROTOCOL_CONFIG[protocol_type].get('boolean_fields')
+    boolean_fields = PROTOCOL_CONFIG[protocol_type].get("boolean_fields")
     for k, v in d.items():
         if k in boolean_fields:
             # replace and 0 or 1 with True or False
-            if v == '0':
+            if v == "0":
                 d[k] = False
-            elif v == '1':
+            elif v == "1":
                 d[k] = True
     return d
+
 
 def get_with_log(name, dictionary, key: str, log_missing=True):
     value = dictionary.get(key)
@@ -61,6 +65,7 @@ def get_with_log(name, dictionary, key: str, log_missing=True):
         LOG.warning(f"Missing key '{key}' in dictionary '{name}'; returning None")
     return None
 
+
 # cached dictionary pattern matches for all responses for each protocol
 def _precompile_response_patterns():
     """Precompile all response patterns"""
@@ -69,13 +74,11 @@ def _precompile_response_patterns():
         patterns = {}
 
         LOG.debug(f"Precompile patterns for {protocol_type}")
-        for name, pattern in config['responses'].items():
-            #LOG.debug(f"Precompiling pattern {name}: {pattern}")
+        for name, pattern in config["responses"].items():
+            # LOG.debug(f"Precompiling pattern {name}: {pattern}")
             patterns[name] = re.compile(pattern)
         precompiled[protocol_type] = patterns
     return precompiled
-
-
 
 
 config_dir = os.path.dirname(__file__)
