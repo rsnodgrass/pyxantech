58c2b7094b328e64e5be1c26f83e16a4d7ae42b7
Ryan Snodgrass <rsnodgrass@gmail.com>
2025-01-05 16:28:44 -0800

    updated version to 0.8.0

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ import os
 import sys
 
 if sys.version_info < (3, 10):
-    raise RuntimeError("This package requres Python 3.10+")
+    raise RuntimeError('This package requres Python 3.10+')
 
-VERSION = "0.7.2"
+VERSION = '0.8.0'
 
 try:
     from setuptools import setup
@@ -14,13 +14,13 @@ except ImportError:
     from distutils.core import setup
 
 
-if sys.argv[-1] == "publish":
-    os.system("python3 setup.py sdist upload")
+if sys.argv[-1] == 'publish':
+    os.system('python3 setup.py sdist upload')
     sys.exit()
 
 setup(
     version=VERSION,
-    download_url="https://github.com/rsnodgrass/pymonoprice/archive/{}.tar.gz".format(
+    download_url='https://github.com/rsnodgrass/pyxantech/archive/{}.tar.gz'.format(
         VERSION
     ),
     include_package_data=True,
